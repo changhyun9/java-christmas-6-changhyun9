29448fb160ee3b15b3ae1bb2237dcac549a76c39
changhyun9 <ckdgus1713@naver.com>
2023-11-13 22:08:12 +0900

    feat : add class of OrderDate

--- src/main/java/christmas/domain/order/OrderDate.java
+++ src/main/java/christmas/domain/order/OrderDate.java
@@ -1,2 +1,29 @@
-package christmas.domain.order;public class OrderDate {
+package christmas.domain.order;
+
+public class OrderDate {
+    public static final int START_DATE = 1;
+    public static final int END_DATE = 31;
+    private int day;
+
+    public OrderDate(String date) {
+        validateNumericType(date);
+        validateRange(date);
+        this.day = Integer.parseInt(date);
+    }
+
+    private void validateNumericType(String date) {
+        try {
+            Integer.parseInt(date);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] Enter only numeric type");
+        }
+    }
+
+    private void validateRange(String date) {
+        int parseDate = Integer.parseInt(date);
+        if (parseDate < START_DATE || parseDate > END_DATE) {
+            throw new IllegalArgumentException("[ERROR] Enter Range 1 ~ 31");
+        }
+    }
+
 }
