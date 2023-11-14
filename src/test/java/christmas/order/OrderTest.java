021d79ca7ab6a9cd511d56b658f7b7c6cadb6d17
changhyun9 <ckdgus1713@naver.com>
2023-11-14 22:32:39 +0900

    docs : check completion of function

---
+++ src/test/java/christmas/order/OrderTest.java
@@ -0,0 +1,25 @@
+package christmas.order;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Order {
+
+    @DisplayName("날짜를 반환한다")
+    @Test
+    void returnDate() {
+
+    }
+
+    @DisplayName("주말 혹은 평일을 반환한다")
+    @Test
+    void returnDayOfWeek() {
+
+    }
+
+    @DisplayName("할인 전 총 주문금액을 반환한다")
+    @Test
+    void returnTotalPriceBeforeDiscount() {
+        
+    }
+}
