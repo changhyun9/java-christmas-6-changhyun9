584ed1dfe138ed62a7b2b5b5abd592912108d319
changhyun9 <ckdgus1713@naver.com>
2023-11-14 22:33:53 +0900

    feat : add class of Order

--- src/main/java/christmas/order/Order.java
+++ src/main/java/christmas/order/Order.java
@@ -1,2 +1,26 @@
-package christmas.order;public class Order {
+package christmas.order;
+
+import christmas.date.OrderDate;
+import christmas.menu.OrderMenus;
+
+public class Order {
+    private final OrderDate orderDate;
+    private final OrderMenus orderMenus;
+
+    public Order(String inputOrderDate, String inputOrderMenus) {
+        this.orderDate = new OrderDate(inputOrderDate);
+        this.orderMenus = new OrderMenus(inputOrderMenus);
+    }
+
+    public int getOrderDate() {
+        return orderDate.getDate();
+    }
+
+    public String getOrderDateOfWeek() {
+        return orderDate.getDayOfWeek();
+    }
+
+    public int getOrderPrice() {
+        return orderMenus.getTotalPrice();
+    }
 }
