3d7923edb1152ea3a32423de295d18b8af90b028
changhyun9 <ckdgus1713@naver.com>
2023-11-15 21:21:23 +0900

    feat : add function to execute program

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,20 @@
 package christmas;
 
+import christmas.service.Service;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Service service = new Service();
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        while (true) {
+            try {
+                service.order();
+                service.printEventPlanner();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+            break;
+        }
     }
 }
